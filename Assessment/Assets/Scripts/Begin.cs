ac7c885489ab31ebd9e90ac70ca81f9d9efc6ad9
apple <apple@appledeMBP.gateway>
2019-08-25 13:15:25 +0800

    add game first

---
+++ Assessment/Assets/Scripts/Begin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Begin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button R;
+    void Start()
+    {
+        R.onClick.AddListener(Re);
+    }
+    public void Re() {
+        SceneManager.LoadScene("SampleScene");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assessment/Assets/Scripts/Sound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+  
+    public AudioSource clicked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+           
+            clicked.Play();
+        }
+    }
+}
