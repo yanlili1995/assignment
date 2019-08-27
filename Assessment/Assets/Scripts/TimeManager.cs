9ea2df6082e79de89b4a7d892fbb3f1d8620d750
apple <apple@appledeMacBook-Pro.local>
2019-08-27 10:59:30 +0800

    Pause Faction

---
+++ Assessment/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    
+    private bool paused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+           {
+            paused = !paused;
+        }
+        if (paused) {
+            Time.timeScale = 0;
+        }
+        else {
+            Time.timeScale = 1;
+        }
+    }
+}
